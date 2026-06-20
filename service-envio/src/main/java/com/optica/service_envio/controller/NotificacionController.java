989c26114a1f5f8fd11d9ab8bef5d5c1c7abbdd6
vicentezepeda <v.zepeda@duocuc.cl>
2026-06-19 23:41:41 -0400

    microServicioEnvio

---
+++ service-envio/src/main/java/com/optica/service_envio/controller/NotificacionController.java
@@ -0,0 +1,30 @@
+package com.optica.service_envio.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.optica.service_envio.model.Notificacion;
+import com.optica.service_envio.repository.NotificacionRepository;
+
+@RestController
+@RequestMapping("/notificaciones")
+public class NotificacionController {
+
+    @Autowired
+    private NotificacionRepository notificacionRepository;
+
+    @PostMapping
+    public Notificacion crear(@RequestBody Notificacion notificacion){
+        return notificacionRepository.save(notificacion);
+    }
+
+    @GetMapping
+    public List<Notificacion> listar(){
+        return notificacionRepository.findAll();
+    }
+}
---
+++ service-envio/src/test/java/com/optica/service_envio/ServiceEnvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optica.service_envio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEnvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
