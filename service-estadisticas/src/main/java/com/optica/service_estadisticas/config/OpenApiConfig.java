dbfabea88bcfe8d5adaaf2f6949a7975608565ef
car-silly <co.becerras@duocuc.cl>
2026-06-30 01:10:08 -0400

    Correcion de clientes, ahora si abre en el swagger y correccion del service auth ahora si puede utilizarse el token en el swagger estadisticas

--- service-estadisticas/src/main/java/com/optica/service_estadisticas/config/OpenApiConfig.java
+++ service-estadisticas/src/main/java/com/optica/service_estadisticas/config/OpenApiConfig.java
@@ -1,7 +1,5 @@
 package com.optica.service_estadisticas.config;
 
-
-
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -9,22 +7,37 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class OpenApiConfig {
 
-
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("API Estadisticas | Servicio de Estadisticas")
-                .version("1.0")
-                .description("Documentación centralizada del Sistema de estadisticas"))
-        
-            .servers(List.of(
-                new Server().url("http://localhost:8081").description("Servidor a través del Gateway")
-            ));
-    }
 
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Estadisticas | Servicio de Estadisticas")
+                        .version("1.0")
+                        .description("Documentación centralizada del Sistema de estadisticas"))
+
+                .servers(List.of(
+                        new Server().url("http://localhost:8081").description("Servidor a través del Gateway")
+                ))
+
+                // Correcion del Et, sistema de seguridad usado en el swagger y no solo en el postman 
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
 }
